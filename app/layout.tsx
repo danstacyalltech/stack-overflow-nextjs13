38d9ced1e1ecdb6336b87ec1f069776714d812cd
YOUR-USERNAME <dan.stacy@alltech.com>
2023-12-01 14:11:18 -0500

    Add assests and edited Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,29 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "Stack Overflow Clone",
+  description:
+    "A community-driven space for developers to ask questions and share knowledge.",
+  icons: {
+    icon: "/assests/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,9 +31,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">This is a piece of text</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
